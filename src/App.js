c237a1b5352b54383c2ad60e214e34ad48eb7426
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-13 19:33:31 +0600

    asdf

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import app from "./firebase/firebase.init";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 
 const auth = getAuth(app);
 function App() {
+  const [user, setUser] = useState({});
   const provider = new GoogleAuthProvider();
 
-  const handleGooleSingIn = () => {
+  const handleGooleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         const user = result.user;
+        setUser(user);
         console.log(user);
       })
       .catch((error) => {
         console.error(error);
       });
   };
+  const handleGooleSignOut = () => {
+    signOut(auth, provider)
+      .then(() => {
+        setUser({});
+      })
+      .catch(() => {
+        setUser({});
+      });
+  };
   return (
     <div>
-      <button onClick={handleGooleSingIn}>Google</button>
+      {user.email ? (
+        <button onClick={handleGooleSignOut}>Sign Out</button>
+      ) : (
+        <button onClick={handleGooleSignIn}>Google</button>
+      )}
+      {user.email && (
+        <div>
+          <h3> User Name: {user.displayName}</h3>
+          <p>Email:{user.email} </p>
+        </div>
+      )}
     </div>
   );
 }
